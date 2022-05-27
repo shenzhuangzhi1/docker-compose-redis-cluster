247dcc4e84a40cfe4828fc4a307b8be9756fc6e9
shenzhuangzhi1 <36810265+shenzhuangzhi1@users.noreply.github.com>
2022-05-27 10:48:31 +0800

    fix: remove password.

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker-compose up -d
-docker exec -it redis-cluster-5 redis-cli --cluster create -a Szzf@2022 redis-cluster-0:6380 redis-cluster-1:6381 redis-cluster-2:6382 redis-cluster-3:6383 redis-cluster-4:6384 redis-cluster-5:6385 --cluster-replicas 1
+docker network inspect docker-compose-redis-cluster_redis-cluster
+docker exec -it redis-cluster-5 redis-cli --cluster create 172.22.0.4:6380 172.22.0.6:6381 172.22.0.5:6382 172.22.0.2:6383 172.22.0.3:6384 172.22.0.7:6385 --cluster-replicas 1
