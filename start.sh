f6f023d6645e003e04b32b5c5d4b21d6bf5cf3c7
shenzhuangzhi1 <36810265+shenzhuangzhi1@users.noreply.github.com>
2022-05-30 13:44:36 +0800

    update: update ship of cluster.

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker-compose up -d
-docker network inspect docker-compose-redis-cluster_redis-cluster
-docker exec -it redis-cluster-5 redis-cli --cluster create 172.22.0.4:6380 172.22.0.6:6381 172.22.0.5:6382 172.22.0.2:6383 172.22.0.3:6384 172.22.0.7:6385 --cluster-replicas 1
+docker network ls
+docker network inspect [docker-compose-redis-cluster_redis-cluster id]
+docker exec -it redis-cluster-5 redis-cli --cluster create 172.21.0.2:6380 172.21.0.3:6381 172.21.0.4:6382 172.21.0.5:6383 172.21.0.6:6384 172.21.0.7:6385 --cluster-replicas 1
