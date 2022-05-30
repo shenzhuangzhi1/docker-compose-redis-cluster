e0cf5ebab0b255336d6606a3b862ceaa681fe4e0
shenzhuangzhi1 <36810265+shenzhuangzhi1@users.noreply.github.com>
2022-05-30 14:34:30 +0800

    update: add password.

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 docker-compose up -d
 docker network ls
 docker network inspect [docker-compose-redis-cluster_redis-cluster id]
-docker exec -it redis-cluster-5 redis-cli --cluster create 172.21.0.2:6380 172.21.0.3:6381 172.21.0.4:6382 172.21.0.5:6383 172.21.0.6:6384 172.21.0.7:6385 --cluster-replicas 1
+docker exec -it redis-cluster-5 redis-cli --cluster create 172.21.0.2:6380 172.21.0.3:6381 172.21.0.4:6382 172.21.0.5:6383 172.21.0.6:6384 172.21.0.7:6385 --cluster-replicas 1 -a Szzf@2022
